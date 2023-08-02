1b8e82ed56df2944b5793aba29ecd3b6235078ad
fairydevmother <smkaraca06@gmail.com>
2023-08-02 06:18:47 +0300

    deployment 7

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import { render } from '@testing-library/react';
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode >
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
